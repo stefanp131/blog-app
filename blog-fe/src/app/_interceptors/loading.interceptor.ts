46f513b48fca7d2d3c881978368d8848822ec4e1
Stefan Popoiu <stefan.popoiu@gmail.com>
2022-11-06 16:12:56 +0200

    make interceptor more usable

--- blog-fe/src/app/_interceptors/loading.interceptor.ts
+++ blog-fe/src/app/_interceptors/loading.interceptor.ts
@@ -3,7 +3,7 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BusyService } from '../_services/busy.service';
@@ -11,15 +11,24 @@ import { delay, finalize } from 'rxjs/operators';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-
   constructor(private busyService: BusyService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (
+      request.method.includes('GET') &&
+      request.url.includes('commentaries')
+    ) {
+      return next.handle(request);
+    }
     this.busyService.busy();
     return next.handle(request).pipe(
+      delay(200),
       finalize(() => {
         this.busyService.idle();
       })
-    )
+    );
   }
 }
